723c1d015d3637bfdf7a4a69f3af054a81798e3a
Natalia <n5@mail.ru>
2020-07-25 21:18:33 +0100

    Completed in-Class exercises

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
-    import React from "react";
-    import Logo from "./Logo"
+   import React from "react";
+   import Logo from "./Logo";
+   import BestPokemon from "./BestPokemon";
+   import CaughtPokemon from "./CaughtPokemon";
 
-    
-
+    // Open the App.js file
+    // Pass a prop appName="Pokedex" to the Logo component
+    // Now open the Logo.js file
+    // Delete the appName variable. What do you see in your web browser? Why?
+    // Change the Logo function to access the first argument and call it props. Use console.log to inspect the props variable
+    // Change the usage of appName in the <h1> to be props.appName instead. Does this fix the problem? Why?
+    // Now open the BestPokemon.js file
+    // Copy the abilities variable and then delete it from BestPokemon.js
+    // Paste the abilities variable into App.js
+    // Pass the abilities variable as a prop to BestPokemon from App.js
+    // In the BestPokemon.js file replace the existing usage of abilities with the abilities prop. You should still see the Pokemon ability names in your web browser
+    // (STRETCH GOAL) Repeat the process with the date variable in the CaughtPokemon.js file
 
     function App(){
       return (
       <div>
-      <Logo />
-      <BestPokemon />
-      <CaughtPokemon />
+      <Logo appName="Pokedex" />
+      <BestPokemon abilities = {['Anticipation', 'Adaptability', 'Run-Away']} />
+      <CaughtPokemon date = {new Date().toLocaleDateString()} />
       </div>
       )
     }
-
-   const CaughtPokemon = () => {
-     const date = new Date().toLocaleDateString();
-     return <p>Caught 0 Pokemon on {date}</p>
-   }
-
-   const BestPokemon = () => {
-     const abilities = ['Anticipation', 'Adaptability', 'Run-Away'];
-     return (
-     <div>
-     <p>My favourite Pokemon is Squirtle</p>
-     <ul>
-       {abilities.map((name) => {
-        return (
-        <li>{name}</li>
-      )
-    })}
-  
-     </ul>
-     </div>
-     )
-   }
    
    export default App;
 
