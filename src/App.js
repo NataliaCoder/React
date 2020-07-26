632756b1c4eaac89991f2d3746f2dd91d08d3871
Natalia <n5@mail.ru>
2020-07-26 11:27:35 +0100

    Completed in-Class exercises

--- src/App.js
+++ src/App.js
@@ -3,19 +3,6 @@
    import BestPokemon from "./BestPokemon";
    import CaughtPokemon from "./CaughtPokemon";
 
-    // Open the App.js file
-    // Pass a prop appName="Pokedex" to the Logo component
-    // Now open the Logo.js file
-    // Delete the appName variable. What do you see in your web browser? Why?
-    // Change the Logo function to access the first argument and call it props. Use console.log to inspect the props variable
-    // Change the usage of appName in the <h1> to be props.appName instead. Does this fix the problem? Why?
-    // Now open the BestPokemon.js file
-    // Copy the abilities variable and then delete it from BestPokemon.js
-    // Paste the abilities variable into App.js
-    // Pass the abilities variable as a prop to BestPokemon from App.js
-    // In the BestPokemon.js file replace the existing usage of abilities with the abilities prop. You should still see the Pokemon ability names in your web browser
-    // (STRETCH GOAL) Repeat the process with the date variable in the CaughtPokemon.js file
-
     function App(){
       return (
       <div>
